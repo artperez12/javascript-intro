24e3c77950751a6488c5b9de48c20546dc7936b5
artperez12 <nooblyy16@gmail.com>
2017-07-11 20:17:06 -0500

    added first js

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+window.onload= function(event) {
+	console.log('hi mom');
+	console.log(document);
+	let ul = document.createElement('ul');
+	console.log(ul);
+
+	document.getelementbyid('list').innerHTML = 'i like cats'
+
+
+
+
+}
